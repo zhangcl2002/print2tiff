4ab6d1bea4b10406e06cd0d3906e28c7dd77b59a
michaelzhang <michaelzhang@aegonthtf.com>
2016-11-16 15:22:03 +0800

    ddd

--- src/main/java/com/aegon/pdf2tiff/APP.java
+++ src/main/java/com/aegon/pdf2tiff/APP.java
@@ -1,11 +1,23 @@
 package com.aegon.pdf2tiff;
 
+
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageOutputStream;
+import javax.media.jai.JAI;
+import javax.media.jai.RenderedOp;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -15,7 +27,13 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 
 import com.sun.javafx.iio.ImageStorage.ImageType;
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import com.sun.media.imageio.plugins.tiff.BaselineTIFFTagSet;
+import com.sun.media.imageio.plugins.tiff.TIFFTag;
+import com.sun.media.jai.codec.ImageCodec;
+import com.sun.media.jai.codec.ImageEncoder;
+import com.sun.media.jai.codec.TIFFEncodeParam;
+//import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import com.sun.media.jai.codec.TIFFField;
 
 public class APP {
 
@@ -67,26 +85,75 @@ public class APP {
         Date start = new Date();  
         PDDocument document = PDDocument.load(new File("c:\\1.pdf"));
               
-        FileOutputStream fo= new FileOutputStream(new File("c:\\xx.tif"));    
+        //FileOutputStream fo= new FileOutputStream(new File("c:\\xx.tif"));    
         
         PDFRenderer pdfRenderer = new PDFRenderer(document);
         int pageCounter = 0;
-    
+        
+        System.out.println(document.getPages());
+        
+        ArrayList<BufferedImage>  bimTab = new ArrayList();
+               
+        
         for (PDPage page : document.getPages())
         {
+        	
             // note that the page number parameter is zero based
-            BufferedImage bim = pdfRenderer.renderImageWithDPI(pageCounter, 300,org.apache.pdfbox.rendering.ImageType.BINARY);
-
+        	BufferedImage bim = pdfRenderer.renderImageWithDPI(pageCounter++, 300,org.apache.pdfbox.rendering.ImageType.BINARY);                
           //  ImageIOUtil.
             // suffix in filename will be used as the file format
-     //       ImageIOUtil.writeImage(bim, "c:\\1" + "-" + (pageCounter++) + ".tif", 300);
-            ImageIOUtil.writeImage(bim, "tif", fo, 300, 0f);
+            bimTab.add(bim);
+           //ImageIOUtil.writeImage(bim, "c:\\1" + "-" + (pageCounter++) + ".tif", 300);
+            //ImageIOUtil.writeImage(bim, "tif", fo, 300, 0f);
+        
+            System.out.println("kkkkkkkkkkkkkkkk");
             
         }
+        
+        new APP().saveAsMultipageTIFF(bimTab,"222.tif",300);
+        
         document.close();
+        //fo.close();
+        /*List<File> srcFile ;
+        try{        
+        srcFile= new ArrayList();
+        srcFile.add(new File("c:\\1-0.tif"));
+        srcFile.add(new File("c:\\1-1.tif"));
+        new APP().tif2Marge(srcFile,"c:\\2.tif");
+        }
+        catch (Exception e){
+        	e.printStackTrace();
+        }*/
         Date end = new Date();  
         System.out.println(end.getTime()-start.getTime());  
         System.out.println("over");  
     }  
+    
+    
+    void saveAsMultipageTIFF(ArrayList<BufferedImage> bimTab, String filename, int dpi) throws IOException
+    {
+        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("tiff");
+        ImageWriter imageWriter = writers.next();
+
+        ImageOutputStream ios = ImageIO.createImageOutputStream(new File(filename));
+        imageWriter.setOutput(ios);
+        imageWriter.prepareWriteSequence(null);
+        for (BufferedImage image : bimTab)
+        {
+            ImageWriteParam param = imageWriter.getDefaultWriteParam();
+            IIOMetadata metadata = imageWriter.getDefaultImageMetadata(new ImageTypeSpecifier(image), param);
+            param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            TIFFUtil.setCompressionType(param, image);
+            TIFFUtil.updateMetadata(metadata, image, dpi);
+            imageWriter.writeToSequence(new IIOImage(image, null, metadata), param);
+        }
+        imageWriter.endWriteSequence();
+        imageWriter.dispose();
+        ios.flush();
+        ios.close();
+    }    
+    
+  
+   
 	
 }
